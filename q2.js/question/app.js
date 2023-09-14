a3d4956662ebac33669e2cae0e4d85c837251881
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-09-14 18:55:15 +0900

    q2 OK

---
+++ q2.js/question/app.js
@@ -0,0 +1,10 @@
+(()=>{
+  const $button = ; //要修正
+  const $modal = ; //要修正
+
+  const clickHandler = () => {
+    $modal.classList.toggle('is-active');
+  };
+
+  $button.addEventListener('click', () => clickHandler());
+})();
